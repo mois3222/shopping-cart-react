1a48ad137d37b99b558ffe9724c6f5332260e878
mois3222 <moisky222@gmail.com>
2023-03-15 21:29:01 -0500

    First commit in the branch developer 15/03/2023

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Context, ContextProvider } from "./context/Context";
+import { ContextProvider } from "./context/Context";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ContextProvider>
